218c79e71a1459df3ca63da2ac248d9057d122fd
Vanessa Formigão Gomes <vanessafmgds@gmail.com>
2023-08-22 14:17:00 -0300

    inserindo novas aulas

---
+++ 1M/PA/calculo_idade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anos, meses, dias, total;
+
+            Console.WriteLine("Informe sua idade em anos:");
+            anos= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe sua idade em meses:");
+            meses= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe sua idade em dias:");
+            dias= int.Parse(Console.ReadLine());
+
+            total = anos * 365 + meses * 30 + dias;
+            Console.WriteLine("O total de dias vividos é: " + total);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1M/PA/converter_moedas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace converter_moedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar, euro, ouro, valor_investido, valor_dolar, valor_euro, valor_ouro;
+
+            Console.WriteLine("Informe a cotação do Dólar:");
+            dolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a cotação do Euro:");
+            euro = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a cotação do Ouro:");
+            ouro = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor a ser investido:");
+            valor_investido = double.Parse(Console.ReadLine());
+
+            valor_dolar = valor_investido / dolar;
+            valor_euro = valor_investido / euro;
+            valor_ouro = valor_investido / ouro;
+
+            Console.WriteLine("Valor em Dólar: " + valor_dolar.ToString("F2"));
+            Console.WriteLine("Valor em Euro: " + valor_euro.ToString("F2"));
+            Console.WriteLine("Valor em Ouro: " + valor_ouro.ToString("F2"));
+
+            Console.ReadKey();
+
+
+
+                
+         }
+    }
+}
